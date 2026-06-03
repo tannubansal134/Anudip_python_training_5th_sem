75f7c082ec78cb28364e968a14d31816a030deb1
tannubansal134 <tannubansal134@gmail.com>
2026-06-03 15:36:58 +0530

    Add files via upload

---
+++ class_work/angel.py
@@ -0,0 +1,19 @@
+angle1 = float(input("enter first angle:"))
+# validate angle1
+if(angle1 <=0):
+    exit("angle must be positive")
+#---------------------------------------
+angle2 = float(input("enter second angle:"))
+#validate angle2
+if(angle2 <= 0):
+    exit("angle must be positive")
+#---------------------------------------
+angle3 = float(input("enter the third angle"))
+#validate angle3
+if(angle3 <= 0):
+    exit("angle must be positive")
+#verifying triangle formation
+if(angle1+angle2+angle==180):
+    print("above angles from a triangle")
+else:
+    print("above angles do not form a triangle")
