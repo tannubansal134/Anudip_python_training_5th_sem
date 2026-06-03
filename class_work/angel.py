ed82ad79e9dea2664e488d1481aa73d6522775fb
tannubansal134 <tannubansal134@gmail.com>
2026-06-03 15:57:45 +0530

    Update angel.py

--- class_work/angel.py
+++ class_work/angel.py
@@ -13,7 +13,7 @@ angle3 = float(input("enter the third angle"))
 if(angle3 <= 0):
     exit("angle must be positive")
 #verifying triangle formation
-if(angle1+angle2+angle==180):
+if(angle1+angle2+angle3==180):
     print("above angles from a triangle")
 else:
     print("above angles do not form a triangle")
