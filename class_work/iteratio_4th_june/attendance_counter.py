c438d71e21f54cc263a3e84739d6d137e30ca0bd
tannubansal134 <tannubansal134@gmail.com>
2026-06-04 13:33:31 +0530

    Add files via upload
    this is my attendance counter.py

---
+++ class_work/iteratio_4th_june/attendance_counter.py
@@ -0,0 +1,10 @@
+# program to displaying the student
+attendance_count = 0
+while(attendance_count <=30):
+    print ("student entered")
+    attendance_count = attendance_count + 1
+    print("attendance count:",attendance_count)
+#------------------------------------------------
+print("all student are present.")
+
+
