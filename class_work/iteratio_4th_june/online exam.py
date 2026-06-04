ee89eaf1a6db88397a8a5f4a8a37cb45e7ba01cb
tannubansal134 <tannubansal134@gmail.com>
2026-06-04 13:30:10 +0530

    Add files via upload
    this is my online exam.py

---
+++ class_work/iteratio_4th_june/online exam.py
@@ -0,0 +1,12 @@
+# program to displaying the result
+marks = 0
+while (marks < 40):
+    marks = int(input("Enter Marks: "))
+
+    if marks < 40:
+        print("Result: Fail")
+    else:
+        print("Result: Pass")
+#------------------------------------------
+
+print("Congratulations! You have cleared the assessment.")
