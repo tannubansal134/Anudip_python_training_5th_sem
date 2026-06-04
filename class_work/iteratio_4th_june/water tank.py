88e2f0a1b5728f27c28d0580dd5f12111a68a6e5
tannubansal134 <tannubansal134@gmail.com>
2026-06-04 13:25:58 +0530

    Add files via upload
    this is my water tank.py

---
+++ class_work/iteratio_4th_june/water tank.py
@@ -0,0 +1,10 @@
+# program to displaying water level
+water_level = 0
+while(water_level <100):
+          water_level = water_level + 10
+          print("water_level:",water_level,"liters")
+#-----------------------------------------==
+print("tank is full")
+
+
+
