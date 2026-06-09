9f4aa5f6a28481916dbc720e9cbb03264408f9f7
tannubansal134 <tannubansal134@gmail.com>
2026-06-09 15:37:34 +0530

    Add files via upload
    this is my calculation.py

---
+++ class_work/function_9th_june/calculation.py
@@ -0,0 +1,10 @@
+#import module numeric calculations
+from numericcalculation import *
+#----------------------------------
+#main program
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+#to calculate addition
+print("Addition : ",add(num1, num2))
+#to calculate subtraction
+print("Difference between",num1," and ",num2,"is : ",subtract(num1, num2))
