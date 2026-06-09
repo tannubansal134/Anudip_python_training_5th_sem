e0479dfd7c9428a9dd2c3c7a169c84bd9cded84e
tannubansal134 <tannubansal134@gmail.com>
2026-06-09 22:07:48 +0530

    Add files via upload
    this is my addition,py

---
+++ class_work/function_9th_june/addition.py
@@ -0,0 +1,8 @@
+#to import numericcalculation module
+from numericcalculation import add
+#----------------------------------
+#main program
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+#to calculate addition
+print("Addition : ",add(num1, num2))
